7a97b722bf051284c6f2c201577de54a763a30c5
vinit <vinit@holidayiq.com>
2017-06-01 12:25:40 +0530

    include only .cpp files

--- codechef/MARCH17/description.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  cout << ":) All programs done in long contest in MARCH 2017 :) ";
-}
--- tests/infinite.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	
-	for(int i = 1; i ;i++ ){
-		cout<<i<<" ";
-	}
-} 
