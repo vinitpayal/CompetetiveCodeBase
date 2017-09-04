b04399844018b42e97b411d1d8903fbcd8b4d986
vinit <vinit@holidayiq.com>
2017-09-04 11:49:02 +0530

    knapsack optimized

--- javaSolutions/src/DP/KnapSack01NaiveRecursive.java
+++ javaSolutions/src/DP/KnapSack01NaiveRecursive.java
@@ -3,7 +3,10 @@ import java.lang.*;
 import java.io.*;
 
 class ActualWorker{
-    public int knapsackRecursive(int[] v, int[] w,int capacity, int size, int currIndex, int currSum){
+
+    public Map<String,Integer> memoize = new HashMap<String,Integer>();
+
+    public int knapsackRecursive(int[] v, int[] w, int capacity, int size, int currIndex, int currSum){
         if(currIndex >= size) return currSum;
 
         int returnVal;
@@ -20,9 +23,30 @@ class ActualWorker{
             returnVal = Math.max(sumAfterIncluding, sumWithoutIncluding);
         }
 
-        // System.out.println("For index :"+currIndex+" returnVal :"+returnVal);
-
         return returnVal;
+    }
+
+    public int knapsackTabulation(int capacity, int size, int[] v, int[] w){
+        int[][] tabulation = new int[size+1][capacity+1];
+
+        for(int i = 0; i <= size; i++){
+            for(int j = 0; j <= capacity; j++){
+                if(i == 0 || j == 0) tabulation[i][j] = 0;
+
+                else if(w[i-1] > j){
+                    tabulation[i][j] = tabulation[i-1][j];
+                }
+
+                else{
+                    int p1 = v[i-1] + tabulation[i-1][j-w[i-1]];
+                    int p2 = tabulation[i-1][j];
+
+                    tabulation[i][j] = Math.max(p1,p2);
+                }
+            }
+        }
+
+        return tabulation[size][capacity];
     }
 }
 
@@ -47,7 +71,7 @@ public class KnapSack01NaiveRecursive{
                 w[i] = sc.nextInt();
             }
 
-            System.out.println(new ActualWorker().knapsackRecursive(v,w,c,n,0,0));
+            System.out.println(new ActualWorker().knapsackTabulation(c,n,v,w));
         }
     }
 }
