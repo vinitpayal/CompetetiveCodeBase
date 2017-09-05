b5eedebf94b5f3850e7f7f7eafd051555b733e9b
vinit <vinit@holidayiq.com>
2017-09-06 00:57:37 +0530

    rotated array find corner case handle

--- javaSolutions/src/array/FindInRotatedArray.java
+++ javaSolutions/src/array/FindInRotatedArray.java
@@ -3,7 +3,7 @@ class ActualWorker{
 //        System.out.println("s:"+start+"e:"+end);
         int mid = (start + end)/2;
 
-        if(start > end) return -1;
+        if(start >= end && arr[start] != element) return -1;
         if(arr[start] == element) return start;
         if(arr[end] == element) return end;
         if(arr[mid] == element) return mid;
@@ -32,7 +32,7 @@ class ActualWorker{
 
 public class FindInRotatedArray{
     public static void main(String[] args) {
-        int[] arr = new int[] {8, 10, 20, 80, 100, 200, 400, 500, 3, 2, 1};
+        int[] arr = new int[] {1, 3, 50, 10, 9, 7, 6};
 
         for(int i = 0; i < arr.length;i++){
             System.out.println(arr[i]+"=> index :"+new ActualWorker().findElementInRotatedArray(arr,arr[i],0,arr.length-1));
