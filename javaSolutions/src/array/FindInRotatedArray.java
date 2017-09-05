fa2ea59b955ae261174d2559c2d66e509c2aa685
vinit <vinit@holidayiq.com>
2017-09-06 00:54:18 +0530

    find an element in sorted rotated array

---
+++ javaSolutions/src/array/FindInRotatedArray.java
@@ -0,0 +1,43 @@
+class ActualWorker{
+    public int findElementInRotatedArray(int[] arr, int element,int start, int end){
+//        System.out.println("s:"+start+"e:"+end);
+        int mid = (start + end)/2;
+
+        if(start > end) return -1;
+        if(arr[start] == element) return start;
+        if(arr[end] == element) return end;
+        if(arr[mid] == element) return mid;
+
+        if(arr[mid] < arr[start]){
+            if(element > arr[start]) return findElementInRotatedArray(arr, element, start, mid);
+            else return findElementInRotatedArray(arr,element,mid+1,end);
+        }
+        else if(arr[mid] > arr[start] && arr[mid] > arr[mid+1]){
+            int n1 = findElementInRotatedArray(arr, element, start, mid);
+            int n2 = findElementInRotatedArray(arr, element, mid+1, end);
+            return Math.max(n1,n2);
+        }
+        else if(arr[mid] > arr[start] && arr[mid] < arr[mid+1]){
+            if(arr[mid] < element){
+                return findElementInRotatedArray(arr, element, mid+1, end);
+            }
+            else{
+                if(element > arr[start]) return findElementInRotatedArray(arr, element, start, mid);
+                else return findElementInRotatedArray(arr,element,mid+1,end);
+            }
+        }
+        return -2;
+    }
+}
+
+public class FindInRotatedArray{
+    public static void main(String[] args) {
+        int[] arr = new int[] {8, 10, 20, 80, 100, 200, 400, 500, 3, 2, 1};
+
+        for(int i = 0; i < arr.length;i++){
+            System.out.println(arr[i]+"=> index :"+new ActualWorker().findElementInRotatedArray(arr,arr[i],0,arr.length-1));
+
+        }
+
+    }
+}
