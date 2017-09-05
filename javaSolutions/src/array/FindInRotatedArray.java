f90612c92f519bd4d8114377ee7c383978a4fa4c
vinit <vinit@holidayiq.com>
2017-09-06 01:15:24 +0530

    test case change for rotated array find

--- javaSolutions/src/array/FindInRotatedArray.java
+++ javaSolutions/src/array/FindInRotatedArray.java
@@ -13,9 +13,8 @@ class ActualWorker{
             else return findElementInRotatedArray(arr,element,mid+1,end);
         }
         else if(arr[mid] > arr[start] && arr[mid] > arr[mid+1]){
-            int n1 = findElementInRotatedArray(arr, element, start, mid);
-            int n2 = findElementInRotatedArray(arr, element, mid+1, end);
-            return Math.max(n1,n2);
+            if(element > arr[start]) return findElementInRotatedArray(arr, element, start, mid);
+            else return findElementInRotatedArray(arr,element,mid+1,end);
         }
         else if(arr[mid] > arr[start] && arr[mid] < arr[mid+1]){
             if(arr[mid] < element){
@@ -32,7 +31,8 @@ class ActualWorker{
 
 public class FindInRotatedArray{
     public static void main(String[] args) {
-        int[] arr = new int[] {1, 3, 50, 10, 9, 7, 6};
+        int[] arr = new int[] {11,13,15,17,19,221,232,56,-17,-3,1,2,4,6,8};
+//        System.out.println("6=> index :"+new ActualWorker().findElementInRotatedArray(arr,6,0,arr.length-1));
 
         for(int i = 0; i < arr.length;i++){
             System.out.println(arr[i]+"=> index :"+new ActualWorker().findElementInRotatedArray(arr,arr[i],0,arr.length-1));
